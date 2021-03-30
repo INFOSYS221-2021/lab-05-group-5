f85f3a72ce1e6960bd452bb64dd46cd494be01f5
rickyxu0221 <80726476+rickyxu0221@users.noreply.github.com>
2021-03-30 14:13:18 +1300

    Update exercise02.py

--- exercise02.py
+++ exercise02.py
@@ -1,4 +1,14 @@
 # Exercise Two
 # Write a simple program that finds the value at the nth position in the Fibonacci sequence
-
-print("The value at the 6th postion in the Fibonacci sequence is 8")
+value = 6
+fibonacci = 1 
+p = 1
+pp = 1
+if value > 2:
+    for i in range(value-2):
+        fibonacci =  p + pp
+        pp = p 
+        p = fibonacci
+  
+  
+print("The value at the " +  str(value) + " postion in the Fibonacci sequence is " + str(fibonacci))
